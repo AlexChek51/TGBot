276b34cf3fbec676d01d011547c3093092b94428
unknown <sasha.checkulin@yandex.ru>
2024-02-29 20:02:02 +0300

    first

---
+++ bot_module.py
@@ -0,0 +1,7 @@
+# bot_module.py
+from aiogram import Bot, Dispatcher
+
+API_TOKEN = '7036651147:AAHgZCE6m72zvpjN5MLKMop0KgldWf66cJU'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
---
+++ main.py
@@ -0,0 +1,28 @@
+# main.py
+import aiosqlite
+import asyncio
+from bot_module import bot, dp
+
+from handlers import cmd_start, cmd_quiz
+
+DB_NAME = 'quiz_bot.db'
+
+async def create_table():
+    # Создаем соединение с базой данных (если она не существует, она будет создана)
+    async with aiosqlite.connect(DB_NAME) as db:
+        # Создаем таблицу
+        await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
+        await db.execute('''CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, score INTEGER)''')
+        # Сохраняем изменения
+        await db.commit()
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+
+    # Запускаем создание таблицы базы данных
+    await create_table()
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
